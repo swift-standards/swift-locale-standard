d06b35e11680b07961a55f39eb59386803162afb
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-03 13:41:50 +0200

    Apply mechanical lint fixes (swift-linter autofix, swift-format, SwiftLint) (#6)
    * Apply mechanical lint fixes (swift-linter autofix, swift-format, SwiftLint)
    
    * Apply swift-linter autofix; clear mechanical SwiftLint residuals

--- Package.swift
+++ Package.swift
@@ -12,7 +12,9 @@ extension String {
 
 extension Target.Dependency {
     static var localeStandard: Self { .target(name: .localeStandard) }
-    static var standards: Self { .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions") }
+    static var standards: Self {
+        .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions")
+    }
     static var bcp47: Self { .product(name: "BCP 47", package: "swift-bcp-47") }
     static var iso639: Self { .product(name: "ISO 639", package: "swift-iso-639") }
     static var iso3166: Self { .product(name: "ISO 3166", package: "swift-iso-3166") }
@@ -25,17 +27,20 @@ let package = Package(
         .macOS(.v26),
         .iOS(.v26),
         .tvOS(.v26),
-        .watchOS(.v26)
+        .watchOS(.v26),
     ],
     products: [
         .library(name: "Locale Standard", targets: ["Locale Standard"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-standard-library-extensions.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-standard-library-extensions.git",
+            branch: "main"
+        ),
         .package(url: "https://github.com/swift-ietf/swift-bcp-47.git", branch: "main"),
         .package(url: "https://github.com/swift-iso/swift-iso-639.git", branch: "main"),
         .package(url: "https://github.com/swift-iso/swift-iso-3166.git", branch: "main"),
-        .package(url: "https://github.com/swift-iso/swift-iso-15924.git", branch: "main")
+        .package(url: "https://github.com/swift-iso/swift-iso-15924.git", branch: "main"),
     ],
     targets: [
         .target(
@@ -45,7 +50,7 @@ let package = Package(
                 .bcp47,
                 .iso639,
                 .iso3166,
-                .iso15924
+                .iso15924,
             ]
         ),
         .testTarget(
@@ -53,7 +58,7 @@ let package = Package(
             dependencies: [
                 "Locale Standard"
             ]
-        )
+        ),
     ],
     swiftLanguageModes: [.v6]
 )
@@ -69,7 +74,7 @@ for target in package.targets where ![.system, .binary, .plugin, .macro].contain
         .enableExperimentalFeature("Lifetimes"),
         .enableExperimentalFeature("SuppressedAssociatedTypes"),
         .enableUpcomingFeature("InferIsolatedConformances"),
-        .enableUpcomingFeature("LifetimeDependence")
+        .enableUpcomingFeature("LifetimeDependence"),
     ]
 
     let package: [SwiftSetting] = []
